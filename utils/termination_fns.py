510201495babfec94148f7a96bf5a1bb2e274dc9
Mohammadreza <mohammadreza.mrn2010@gmail.com>
2024-05-03 13:22:06 +0300

    change to np

--- utils/termination_fns.py
+++ utils/termination_fns.py
@@ -1,4 +1,4 @@
-import torch
+import numpy as np
 
 def obs_unnormalization(termination_fn, obs_mean, obs_std):
     def thunk(obs, act, next_obs):
@@ -10,7 +10,7 @@ def obs_unnormalization(termination_fn, obs_mean, obs_std):
 def termination_fn_halfcheetah(obs, act, next_obs):
     assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
-    not_done = torch.logical_and(torch.all(next_obs > -100, axis=-1), torch.all(next_obs < 100, axis=-1))
+    not_done = np.logical_and(np.all(next_obs > -100, axis=-1), np.all(next_obs < 100, axis=-1))
     done = ~not_done
     done = done[:, None]
     return done
@@ -20,10 +20,10 @@ def termination_fn_hopper(obs, act, next_obs):
 
     height = next_obs[:, 0]
     angle = next_obs[:, 1]
-    not_done =  torch.isfinite(next_obs).all(axis=-1) \
-                    * torch.abs(next_obs[:,1:] < 100).all(axis=-1) \
+    not_done =  np.isfinite(next_obs).all(axis=-1) \
+                    * np.abs(next_obs[:,1:] < 100).all(axis=-1) \
                     * (height > .7) \
-                    * (torch.abs(angle) < .2)
+                    * (np.abs(angle) < .2)
 
     done = ~not_done
     done = done[:,None]
@@ -32,7 +32,7 @@ def termination_fn_hopper(obs, act, next_obs):
 def termination_fn_halfcheetahveljump(obs, act, next_obs):
     assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
-    done = torch.array([False]).repeat(len(obs))
+    done = np.array([False]).repeat(len(obs))
     done = done[:,None]
     return done
 
@@ -40,7 +40,7 @@ def termination_fn_antangle(obs, act, next_obs):
     assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
     x = next_obs[:, 0]
-    not_done = 	torch.isfinite(next_obs).all(axis=-1) \
+    not_done = 	np.isfinite(next_obs).all(axis=-1) \
                 * (x >= 0.2) \
                 * (x <= 1.0)
 
@@ -52,7 +52,7 @@ def termination_fn_ant(obs, act, next_obs):
     assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
     x = next_obs[:, 0]
-    not_done = 	torch.isfinite(next_obs).all(axis=-1) \
+    not_done = 	np.isfinite(next_obs).all(axis=-1) \
                 * (x >= 0.2) \
                 * (x <= 1.0)
 
@@ -65,7 +65,7 @@ def termination_fn_walker2d(obs, act, next_obs):
 
     height = next_obs[:, 0]
     angle = next_obs[:, 1]
-    not_done =  torch.logical_and(torch.all(next_obs > -100, axis=-1), torch.all(next_obs < 100, axis=-1)) \
+    not_done =  np.logical_and(np.all(next_obs > -100, axis=-1), np.all(next_obs < 100, axis=-1)) \
                 * (height > 0.8) \
                 * (height < 2.0) \
                 * (angle > -1.0) \
@@ -77,21 +77,21 @@ def termination_fn_walker2d(obs, act, next_obs):
 def termination_fn_point2denv(obs, act, next_obs):
     assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
-    done = torch.array([False]).repeat(len(obs))
+    done = np.array([False]).repeat(len(obs))
     done = done[:,None]
     return done
 
 def termination_fn_point2dwallenv(obs, act, next_obs):
     assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
-    done = torch.array([False]).repeat(len(obs))
+    done = np.array([False]).repeat(len(obs))
     done = done[:,None]
     return done
 
 def termination_fn_pendulum(obs, act, next_obs):
     assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
-    done = torch.zeros((len(obs), 1))
+    done = np.zeros((len(obs), 1))
     return done
 
 def termination_fn_humanoid(obs, act, next_obs):
@@ -115,7 +115,7 @@ def termination_fn_pen(obs, act, next_obs):
 def terminaltion_fn_door(obs, act, next_obs):
     assert len(obs.shape) == len(next_obs.shape) == len(act.shape) == 2
 
-    done = torch.array([False] * obs.shape[0])
+    done = np.array([False] * obs.shape[0])
 
     done = done[:, None]
     return done
@@ -146,4 +146,4 @@ def get_termination_fn(task):
     elif 'door' in task:
         return terminaltion_fn_door
     else:
-        raise torch.zeros
+        raise np.zeros
